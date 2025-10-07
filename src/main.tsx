8ec9768fbff0df118cfa257b8e2fb95ef77bce76
Right Notes Wrong Order <rightnoteswrongorder@gmail.com>
2025-10-07 11:26:52 +0100

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { registerSW } from "./sw-register";
 
-registerSW();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
